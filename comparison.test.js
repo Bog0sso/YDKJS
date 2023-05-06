7dc300044438087797c83b374f8027dc88b128d5
Bog0sso <sambe2000@hotmail.com>
2023-05-06 18:07:02 +0000

    QA: comparison.test & closure.test fully implemented

--- comparison.test.js
+++ comparison.test.js
@@ -1,4 +1,53 @@
-import {  } from "module";
-test('test addition',()=>{
-    console.log(1+1)
+const { scheduleMeeting } = require("./comparison.js");
+test(' DAY_START = 07:30, DAY_END=17:45 ----scheduleMeeting("7:00",15) should return false',()=>{
+    const DAY_START = "07:30";
+    const DAY_END   = "17:45";
+    // expect(scheduleMeeting("7:00",15)).toBe(true);
+    expect(scheduleMeeting("7:00",15)).toBe(false)
+
+})
+
+
+test(' DAY_START = 07:30, DAY_END=17:45 ----scheduleMeeting("7:15",30) should return false',()=>{
+    const DAY_START = "07:30";
+    const DAY_END   = "17:45";
+    expect(scheduleMeeting("07:15",30)).toBe(false)
+
+})
+
+test(' DAY_START = 07:30, DAY_END=17:45 ----scheduleMeeting("7:15",30) should return true',()=>{
+    const DAY_START = "07:30";
+    const DAY_END   = "17:45";
+    expect(scheduleMeeting("7:30",30)).toBe(true)
+
+})
+
+test(' DAY_START = 07:30, DAY_END=17:45 ----scheduleMeeting("11:30",60) should return true',()=>{
+    const DAY_START = "07:30";
+    const DAY_END   = "17:45";
+    expect(scheduleMeeting("11:30",60)).toBe(true)
+})
+
+test(' DAY_START = 07:30, DAY_END=17:45 ----scheduleMeeting("17:00",45) should return true',()=>{
+    const DAY_START = "07:30";
+    const DAY_END   = "17:45";
+    expect(scheduleMeeting("17:00",45)).toBe(true)
+    
+})
+
+
+test(' DAY_START = 07:30, DAY_END=17:45 ----scheduleMeeting("17:30",30) should return false',()=>{
+    const DAY_START = "07:30";
+    const DAY_END   = "17:45";
+    expect(scheduleMeeting("17:30",30)).toBe(false)
+    
+})
+
+
+
+test(' DAY_START = 07:30, DAY_END=17:45 ----scheduleMeeting("18:00",15) should return false',()=>{
+    const DAY_START = "07:30";
+    const DAY_END   = "17:45";
+    expect(scheduleMeeting("18:00",15)).toBe(false)
+    
 })
